3c87ed2c83087dcf23f860f4802d28490b3f1d1c
Mac-NotTheCosmetics <macdrahcir.cachapero@edu.sait.ca>
2023-12-06 21:22:28 -0700

    Create main java.js

---
+++ main java.js
@@ -0,0 +1,25 @@
+//Footer//
+const d = new Date() 
+document.getElementById("date").innerHTML = d;
+
+//Alert//
+function btnAlert() {
+  alert('Hi');
+  console.log('The page says "Hi"')
+}
+
+//Hover//
+document.getElementById('hoverButton').onmouseover = () => {
+    document.getElementById('hoverButton').innerText = 'You have been bamboozled lol';
+};
+
+document.getElementById('hoverButton').onmouseleave = () => {
+    document.getElementById('hoverButton').innerText = 'Hover me to get 1 million dollars!!!!!';
+}
+
+//Increment//
+let count = 0
+document.getElementById('btnCounter').onclick = () => {
+  count = count + 1;
+  document.getElementById('txtCounter').innerHTML = count
+}
